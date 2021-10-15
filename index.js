c6f2b7b322eda6acb9eeb096a89aa497792bfa9e
Alice-Retz <alicegretz@gmail.com>
2021-10-15 15:05:24 -0500

    Discord side functiong

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const client = new Discord.Client({
+  intents: [
+    Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+    Discord.Intents.FLAGS.GUILDS,
+    Discord.Intents.FLAGS.GUILD_MESSAGES,
+  ],
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('message', (msg) => {
+  if (msg.content === '>dungeons') {
+    msg.reply('AND dragons');
+  }
+});
+
+client.login(process.env.BARDBOT_TOKEN);
