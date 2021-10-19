0b462e1d9fadf4ccb35a95850b4698159011efcb
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-10-19 14:27:30 -0700

    pushing changes in test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,17 +2,36 @@ const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
+const router = require('../lib/controllers/router.js');
+const Discord = require('discord.js');
+const Message = require('discord.js');
+console.log('HEYYYYY', Message);
+require('dotenv').config();
+const token = process.env.BARDBOT_TOKEN;
+const client = new Discord.Client();
+
+
+
+
 
 describe('bardbot routes', () => {
+
+
+
   beforeEach(() => {
-    return setup(pool);
+ 
+  }
+  );
+
+  it('Check to see if the \'Hello\' message can send ', async() => {
+   
+
+    expect().toEqual();
   });
 
-  it('Check to see if the message has play and by genre ', async() => {
-    const res = await 
-  });
 
-  afterAll(() => {
-    pool.end();
-  });
+  
+  // afterAll(() => {
+  //   pool.end();
+  // });
 });
