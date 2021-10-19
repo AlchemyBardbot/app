c5deac9f24446394648918bd92a24fc5d7b1ef25
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-10-19 16:23:57 -0700

    pushing changes on dylan branch

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,8 @@
-const pool = require('../lib/utils/pool.js');
-const setup = require('../data/setup.js');
-const request = require('supertest');
-const app = require('../lib/app.js');
 const router = require('../lib/controllers/router.js');
-const Discord = require('discord.js');
-const Message = require('discord.js');
-console.log('HEYYYYY', Message);
-require('dotenv').config();
-const token = process.env.BARDBOT_TOKEN;
-const client = new Discord.Client();
+
+
+
+
 
 
 
@@ -18,20 +12,16 @@ describe('bardbot routes', () => {
 
 
 
+
   beforeEach(() => {
- 
+    jest.clearAllMocks();
   }
   );
 
   it('Check to see if the \'Hello\' message can send ', async() => {
-   
 
-    expect().toEqual();
   });
 
 
   
-  // afterAll(() => {
-  //   pool.end();
-  // });
 });
