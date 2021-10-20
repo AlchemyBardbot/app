7ffe90308a3d11bad28a05e469667ccd388772e0
Alice Retz <alicegretz@gmail.com>
2021-10-20 15:51:40 -0500

    Alice (#3)
    * pushing changes and added assets/sounds folders
    
    * setting up play
    
    * Bardbot is playing from static track
    
    * random play function working
    
    * push
    
    * clean up
    
    * Play and stop functionality working!
    
    * Fixed audio quality issue
    
    * allow multiple guilds to use the app at the same time
    
    * seed data added
    
    * Play working as intended
    
    * Setting up the heroku
    
    * Renamed the profile
    
    * test for get a track by genre passing
    
    * Test troubleshooting
    
    * Test troubleshooting
    
    * Test troubleshooting
    
    Co-authored-by: Dylan-Greathouse <Greathouse.dylanj@gmail.com>
    Co-authored-by: greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
    Co-authored-by: Paul Brubaker <pbrubak@gmail.com>

---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+require('dotenv').config();
+const token = process.env.BARDBOT_TOKEN;
+const router = require('./lib/controllers/router.js');
+
+const client = new Discord.Client();
+const connections = {};
+
+client.once('ready', () => {
+    console.log('Roll for initiative!');
+});
+
+client.on('message', async (message) => {
+    if (message.author.bot) return;
+    const messageParts = message.content.split(' ');
+    const keyword = messageParts[0];
+    const args = messageParts.slice(1);
+    const id = message.guild.id;
+    const connection = await router(keyword, args, message, connections[id]);
+    connections[id] = connection;
+});
+
+client.login(token);
