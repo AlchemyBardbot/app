cbcb1bc1d698928bd70ecd6c3530bdb227ce3db9
Paul Brubaker <pbrubak@gmail.com>
2021-10-20 17:50:48 -0700

    CI experiment

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,16 @@
 const router = require('../lib/controllers/router.js');
 const MusicService = require('../lib/services/MusicService.js');
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const jsonToChat = require('../lib/utils/jsonToChat.js');
 
 describe('bardbot routes', () => {
-    afterAll(() => {
-        jest.restoreAllMocks;
+    beforeEach(() => {
+        return setup(pool);
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
     });
 
     jest.spyOn(MusicService, 'play').mockImplementation(() => true);
@@ -31,6 +38,32 @@ describe('bardbot routes', () => {
         await router('!play', ['battle'], message);
         expect(message.channel.send).toHaveBeenCalledWith(
             expect.stringContaining('Now playing')
+        );
+    });
+
+    it('should get all genres and count their tracks', async () => {
+        await router('!genres', [], message);
+        expect(message.channel.send).toHaveBeenCalledWith(
+            expect.stringContaining('travel:')
+        );
+    });
+
+    it('should match!', () => {
+        const input = [
+            { genre: 'town', count: '3' },
+            { genre: 'battle', count: '3' },
+            { genre: 'tavern', count: '3' },
+            { genre: 'travel', count: '3' },
+            { genre: 'magictown', count: '3' },
+        ];
+
+        const output = jsonToChat(input);
+        expect(output).toEqual(
+            `town: 3
+battle: 3
+tavern: 3
+travel: 3
+magictown: 3`
         );
     });
 });
--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const Discord = require('discord.js');
 require('dotenv').config();
 const token = process.env.BARDBOT_TOKEN;
+const prefix = process.env.PREFIX;
 const router = require('./lib/controllers/router.js');
 
 const client = new Discord.Client();
 const connections = {};
 
 client.once('ready', () => {
-  console.log('Roll for initiative!');
+    console.log('Roll for initiative!');
 });
 
 client.on('message', async (message) => {
-  if (message.author.bot) return;
-  const messageParts = message.content.split(' ');
-  const keyword = messageParts[0];
-  const args = messageParts.slice(1);
-  const id = message.guild.id;
-  const connection = await router(keyword, args, message, connections[id]);
-  connections[id] = connection;
+    if (message.author.bot) return;
+    const messageParts = message.content.split(' ');
+    const keyword = messageParts[0];
+    const args = messageParts.slice(1) || [];
+    const id = message.guild.id;
+    const connection = await router(keyword, args, message, connections[id]);
+    if (keyword === `${prefix}play`) connections[id] = connection;
 });
 
 client.login(token);
